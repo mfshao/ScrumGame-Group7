d283c4ce5dd24aaea12bf9cec408d0f521b6cd05
Adam Gruszczynski <adam814176@gmail.com>
2017-10-15 13:00:44 -0500

    Deck creation v1 - testing creation

--- CardDeckModule.java
+++ CardDeckModule.java
@@ -28,10 +28,26 @@ public class CardDeckModule extends Observable implements Observer{
 		JFrame frame = new JFrame("Card Deck Module");
         JPanel panel = new JPanel();
 		//change layout as needed
-        panel.setLayout(new FlowLayout());
+
+  		panel.setLayout(new FlowLayout());
+
+
+        JButton deckOneButton = new JButton("Deck One");
+        JButton deckTwoButton = new JButton("Deck Two");
+        JButton deckThreeButton = new JButton("Deck Three");
+
+        panel.add(deckOneButton);
+        panel.add(deckTwoButton);
+        panel.add(deckThreeButton);
+
+        
+
         frame.add(panel);
         frame.setSize(600, 300);
         frame.setLocation(0,600);
+
+
+		
         //might be messy coding show/hiding these windows(?)
         frame.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
         frame.setVisible(true);
@@ -43,5 +59,7 @@ public class CardDeckModule extends Observable implements Observer{
 		//check for type of o to know what module sent message
 		System.out.println("CardDeck received message: " + arg.toString());
 	}
+
+
 	
 }
