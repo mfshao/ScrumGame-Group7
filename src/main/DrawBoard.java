d0816d2f914de9385f7d5133e40e5d4af600843d
frankdel <frankdel85@gmail.com>
2017-11-01 20:54:49 -0500

    added borad draw based on borad size

--- src/main/DrawBoard.java
+++ src/main/DrawBoard.java
@@ -6,17 +6,67 @@ import java.io.File;
 
 public class DrawBoard extends JPanel
 {
-	private static final String GAME_BOARD_IMAGE_PATH = "/images/Hell board.jpg";
-
+	private String boardLength = "";
+	private String src = "";
+	
 	public DrawBoard()
 	{
+		boardLength =
+		ConfigurationManager.getConfigurationManager().getConfiguration().getBoardLenght();
+		
+		switch(boardLength.toLowerCase()) 
+		{
+			case "short":
+				this.drawShort();
+				break;
+			case "medium":
+				this.drawMed();
+				break;
+			case "long":
+				this.drawLong();
+				break;
+			default:
+				this.drawShort();
+				break;
+		}
+		
+		System.out.println("FILE IN " + src);
+		
+	}
+	
+	private void drawShort()
+	{
+		src = "/images/Short.png";
+		
 		try{
-		BufferedImage image = ImageIO.read(getClass().getResource(GAME_BOARD_IMAGE_PATH));
+		BufferedImage image = ImageIO.read(new File("/images/Short.png"));
+		JLabel label = new JLabel(new ImageIcon(image));
+		
+		this.add(label);
+		} catch (IOException e){
+			
+		}
+	}
+	
+	private void drawMed()
+	{
+		try{
+		BufferedImage image = ImageIO.read(new File("/images/Medium.png"));
 		JLabel label = new JLabel(new ImageIcon(image));
 		this.add(label);
 		} catch (IOException e){
-
+			
 		}
 	}
-
+	
+	private void drawLong()
+	{
+		try{
+		BufferedImage image = ImageIO.read(new File("/images/Long.png"));
+		JLabel label = new JLabel(new ImageIcon(image));
+		this.add(label);
+		} catch (IOException e){
+			
+		}
+	}
 }
